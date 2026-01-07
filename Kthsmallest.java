da8da3d8afd47338f4b778d27b472cab4c5fbf64
thavasri11 <thavasri2006@gmail.com>
2026-01-07 12:05:24 +0530

    Added  Kthsmallest element

---
+++ Kthsmallest.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class KthSmallest {
+    public static void main(String[] args) {
+        int[] arr = {12, 3, 5, 7, 19};
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter K: ");
+        int k = sc.nextInt();
+
+        if (k > 0 && k <= arr.length) {
+            Arrays.sort(arr);
+            System.out.println("Kth smallest element is: " + arr[k - 1]);
+        } else {
+            System.out.println("Invalid value of K");
+        }
+    }
+}
